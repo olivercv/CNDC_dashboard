88c4c86d53b89a824e9ae0b15859c35a6aaad748
olivercv <oliver_c_v@hotmail.com>
2025-02-27 18:19:50 -0400

    bundle page 15

--- src/app/map-centrales/map-centrales.component.ts
+++ src/app/map-centrales/map-centrales.component.ts
@@ -8,33 +8,34 @@ import { Component, OnInit } from '@angular/core';
 export class MapCentralesComponent implements OnInit {
 
   circleData = [
-    { "cx": 337.24, "cy": 306.70, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador A - Potencia Activa: 2.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 2.800 MW" },
-          { "cx": 210.73, "cy": 298.18, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador B - Potencia Activa: 1.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 2.800 MW" },
-          { "cx": 175.35, "cy": 395.38, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador C - Potencia Activa: 6.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 3.100 MW" },
-          { "cx": 289.64, "cy": 475.68, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador D - Potencia Activa: 1.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 1.500 MW" },
-          { "cx": 359.48, "cy": 448.89, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador E - Potencia Activa: 4.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 3.600 MW" },
-          { "cx": 344.36, "cy": 558.00, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador F - Potencia Activa: 3.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 6.700 MW" },
-          { "cx": 414.50, "cy": 690.23, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador G - Potencia Activa: 9.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 5.300 MW" },
-          { "cx": 468.25, "cy": 461.37, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador H - Potencia Activa: 5.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 3.400 MW" },
-          { "cx": 462.65, "cy": 475.68, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador I - Potencia Activa: 6.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 7.500 MW" },
-          { "cx": 475.25, "cy": 500.35, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador J - Potencia Activa: 3.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 4.600 MW" },
-          { "cx": 464.65, "cy": 491.35, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador K - Potencia Activa: 6.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 3.800 MW" },
-          { "cx": 163.02, "cy": 366.00, "r": 7, "fill": "#38b6ff", "class": "circle blue", "dataInfo": "Generador L - Potencia Activa: 8.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 9.300 MW" },
-          { "cx": 195.05, "cy": 381.34, "r": 7, "fill": "#38b6ff", "class": "circle blue", "dataInfo": "Generador M - Potencia Activa: 7.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 5.200 MW" },
-          { "cx": 211.73, "cy": 423.75, "r": 7, "fill": "#38b6ff", "class": "circle blue", "dataInfo": "Generador O - Potencia Activa: 4.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 6.000 MW" },
-          { "cx": 240.83, "cy": 449.63, "r": 7, "fill": "#38b6ff", "class": "circle blue", "dataInfo": "Generador P - Potencia Activa: 3.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 7.100 MW" },
-          { "cx": 276.67, "cy": 455.22, "r": 7, "fill": "#38b6ff", "class": "circle blue", "dataInfo": "Generador Q - Potencia Activa: 2.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 8.300 MW" },
-          { "cx": 292.14, "cy": 455.22, "r": 7, "fill": "#38b6ff", "class": "circle blue", "dataInfo": "Generador L - Potencia Activa: 4.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 3.500 MW" },
-          { "cx": 299.47, "cy": 446.81, "r": 7, "fill": "#38b6ff", "class": "circle blue", "dataInfo": "Generador M - Potencia Activa: 6.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 7.8=600 MW" },
-          { "cx": 270.25, "cy": 446.81, "r": 7, "fill": "#38b6ff", "class": "circle blue", "dataInfo": "Generador N - Potencia Activa: 4.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 5.800 MW" },
-          { "cx": 369.22, "cy": 673.02, "r": 7, "fill": "#38b6ff", "class": "circle blue", "dataInfo": "Generador O - Potencia Activa: 3.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 4.600 MW" },
-          { "cx": 226.82, "cy": 485.75, "r": 7, "fill": "#ffde59", "class": "circle yellow", "dataInfo": "Generador P - Potencia Activa: 1.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 3.200 MW" },
-          { "cx": 232.42, "cy": 655.88, "r": 7, "fill": "#ffde59", "class": "circle yellow", "dataInfo": "Generador Q - Potencia Activa: 8.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 2.900 MW" },
-          { "cx": 367.49, "cy": 691.95, "r": 7, "fill": "#ffde59", "class": "circle yellow", "dataInfo": "Generador R - Potencia Activa: 6.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 1.400 MW" },
-          { "cx": 327.90, "cy": 475.70, "r": 7, "fill": "#7ed957", "class": "circle green", "dataInfo": "Generador S - Potencia Activa: 3.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 6.300 MW" },
-          { "cx": 468.71, "cy": 538.27, "r": 7, "fill": "#7ed957", "class": "circle green", "dataInfo": "Generador T - Potencia Activa: 1.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 1.500 MW" },
-          { "cx": 496.70, "cy": 524.02, "r": 7, "fill": "#7ed957", "class": "circle green", "dataInfo": "Generador U - Potencia Activa: 5.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 5.600 MW" },
-          { "cx": 502.29, "cy": 496.95, "r": 7, "fill": "#7ed957", "class": "circle green", "dataInfo": "Generador V - Potencia Activa: 2.000 MW - Potencia reactiva: 9000 Mvar - Capacidad instalada: 3.500 MW" }
+    { "cx": 337.24, "cy": 306.70, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador A", "potencia_activa": "2.000 MW", "potencia_reactiva": "1.000 Mvar", "capacidad_instalada": "3.000 MW"},
+    { "cx": 210.73, "cy": 298.18, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador B", "potencia_activa": "3.000 MW", "potencia_reactiva": "2.000 Mvar", "capacidad_instalada": "5.000 MW" },
+    { "cx": 175.35, "cy": 395.38, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador C", "potencia_activa": "4.000 MW", "potencia_reactiva": "3.000 Mvar", "capacidad_instalada": "6.000 MW" },
+    { "cx": 289.64, "cy": 475.68, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador D", "potencia_activa": "1.000 MW", "potencia_reactiva": "1.000 Mvar", "capacidad_instalada": "7.000 MW" },
+    { "cx": 359.48, "cy": 448.89, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador E", "potencia_activa": "5.000 MW", "potencia_reactiva": "5.000 Mvar", "capacidad_instalada": "10.000 MW" },
+    { "cx": 344.36, "cy": 558.00, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador F", "potencia_activa": "3.000 MW", "potencia_reactiva": "2.000 Mvar", "capacidad_instalada": "6.000 MW" },
+    { "cx": 414.50, "cy": 690.23, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador G", "potencia_activa": "1.000 MW", "potencia_reactiva": "3.000 Mvar", "capacidad_instalada": "7.000 MW" },
+    { "cx": 468.25, "cy": 461.37, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador H", "potencia_activa": "2.000 MW", "potencia_reactiva": "1.000 Mvar", "capacidad_instalada": "8.000 MW" },
+    { "cx": 462.65, "cy": 475.68, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador I", "potencia_activa": "6.000 MW", "potencia_reactiva": "6.000 Mvar", "capacidad_instalada": "6.000 MW" },
+    { "cx": 475.25, "cy": 500.35, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador J", "potencia_activa": "3.000 MW", "potencia_reactiva": "8.000 Mvar", "capacidad_instalada": "3.000 MW" },
+    { "cx": 464.65, "cy": 491.35, "r": 7, "fill": "#ff3131", "class": "circle red", "dataInfo": "Generador K", "potencia_activa": "2.000 MW", "potencia_reactiva": "1.000 Mvar", "capacidad_instalada": "8.000 MW" },
+    { "cx": 163.02, "cy": 366.00, "r": 7, "fill": "#38b6ff", "class": "circle blue", "dataInfo": "Generador L", "potencia_activa": "1.000 MW", "potencia_reactiva": "2.000 Mvar", "capacidad_instalada": "9.000 MW" },
+    { "cx": 195.05, "cy": 381.34, "r": 7, "fill": "#38b6ff", "class": "circle blue", "dataInfo": "Generador M", "potencia_activa": "2.000 MW", "potencia_reactiva": "2.000 Mvar", "capacidad_instalada": "3.000 MW" },
+    { "cx": 211.73, "cy": 423.75, "r": 7, "fill": "#38b6ff", "class": "circle blue", "dataInfo": "Generador O", "potencia_activa": "4.000 MW", "potencia_reactiva": "5.000 Mvar", "capacidad_instalada": "11.000 MW" },
+    { "cx": 240.83, "cy": 449.63, "r": 7, "fill": "#38b6ff", "class": "circle blue", "dataInfo": "Generador P", "potencia_activa": "4.000 MW", "potencia_reactiva": "2.000 Mvar", "capacidad_instalada": "13.000 MW" },
+    { "cx": 276.67, "cy": 455.22, "r": 7, "fill": "#38b6ff", "class": "circle blue", "dataInfo": "Generador Q", "potencia_activa": "2.000 MW", "potencia_reactiva": "1.000 Mvar", "capacidad_instalada": "4.000 MW" },
+    { "cx": 292.14, "cy": 455.22, "r": 7, "fill": "#38b6ff", "class": "circle blue", "dataInfo": "Generador L", "potencia_activa": "1.000 MW", "potencia_reactiva": "6.000 Mvar", "capacidad_instalada": "4.000 MW" },
+    { "cx": 299.47, "cy": 446.81, "r": 7, "fill": "#38b6ff", "class": "circle blue", "dataInfo": "Generador M", "potencia_activa": "3.000 MW", "potencia_reactiva": "2.000 Mvar", "capacidad_instalada": "5.000 MW" },
+    { "cx": 270.25, "cy": 446.81, "r": 7, "fill": "#38b6ff", "class": "circle blue", "dataInfo": "Generador N", "potencia_activa": "6.000 MW", "potencia_reactiva": "5.000 Mvar", "capacidad_instalada": "6.000 MW" },
+    { "cx": 369.22, "cy": 673.02, "r": 7, "fill": "#38b6ff", "class": "circle blue", "dataInfo": "Generador O", "potencia_activa": "3.000 MW", "potencia_reactiva": "1.000 Mvar", "capacidad_instalada": "8.000 MW" },
+    { "cx": 226.82, "cy": 485.75, "r": 7, "fill": "#ffde59", "class": "circle yellow", "dataInfo": "Generador P", "potencia_activa": "4.000 MW", "potencia_reactiva": "1.000 Mvar", "capacidad_instalada": "9.000 MW" },
+    { "cx": 232.42, "cy": 655.88, "r": 7, "fill": "#ffde59", "class": "circle yellow", "dataInfo": "Generador Q", "potencia_activa": "5.000 MW", "potencia_reactiva": "5.000 Mvar", "capacidad_instalada": "7.000 MW" },
+    { "cx": 367.49, "cy": 691.95, "r": 7, "fill": "#ffde59", "class": "circle yellow", "dataInfo": "Generador R", "potencia_activa": "5.000 MW", "potencia_reactiva": "2.000 Mvar", "capacidad_instalada": "8.000 MW" },
+    { "cx": 327.90, "cy": 475.70, "r": 7, "fill": "#7ed957", "class": "circle green", "dataInfo": "Generador S", "potencia_activa": "2.000 MW", "potencia_reactiva": "3.000 Mvar", "capacidad_instalada": "9.000 MW" },
+    { "cx": 468.71, "cy": 538.27, "r": 7, "fill": "#7ed957", "class": "circle green", "dataInfo": "Generador T", "potencia_activa": "1.000 MW", "potencia_reactiva": "1.000 Mvar", "capacidad_instalada": "6.000 MW" },
+    { "cx": 496.70, "cy": 524.02, "r": 7, "fill": "#7ed957", "class": "circle green", "dataInfo": "Generador U", "potencia_activa": "3.000 MW", "potencia_reactiva": "2.000 Mvar", "capacidad_instalada": "5.000 MW" },
+    { "cx": 502.29, "cy": 496.95, "r": 7, "fill": "#7ed957", "class": "circle green", "dataInfo": "Generador V", "potencia_activa": "6.000 MW", "potencia_reactiva": "1.000 Mvar", "capacidad_instalada": "8.000 MW" },
+    { "cx": 415.29, "cy": 496.95, "r": 7, "fill": "#c634eb", "class": "circle purple", "dataInfo": "Generador w", "potencia_activa": "6.000 MW", "potencia_reactiva": "1.000 Mvar", "capacidad_instalada": "8.000 MW" }
   ];
 
   buttons = [
@@ -42,6 +43,7 @@ export class MapCentralesComponent implements OnInit {
     { id: "btn-blue", colorClass: "blue" },
     { id: "btn-yellow", colorClass: "yellow" },
     { id: "btn-green", colorClass: "green" },
+    { id: "btn-purple", colorClass: "purple" },
   ];
 
   constructor() { }
@@ -53,7 +55,7 @@ export class MapCentralesComponent implements OnInit {
 
   createCircles(): void {
     const svg = document.querySelector('#map-svg');
-    this.circleData.forEach(({ cx, cy, r, fill, class: className, dataInfo }) => {
+    this.circleData.forEach(({ cx, cy, r, fill, class: className, dataInfo, potencia_activa, potencia_reactiva, capacidad_instalada }) => {
       const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
       circle.setAttribute("cx", cx.toString());
       circle.setAttribute("cy", cy.toString());
@@ -61,6 +63,9 @@ export class MapCentralesComponent implements OnInit {
       circle.setAttribute("fill", fill);
       circle.setAttribute("class", className);
       circle.setAttribute("data-info", dataInfo);
+      circle.setAttribute("potencia_activa", potencia_activa);
+      circle.setAttribute("potencia_reactiva", potencia_reactiva);
+      circle.setAttribute("capacidad_instalada", capacidad_instalada);
       svg?.appendChild(circle);
     });
   }
@@ -74,8 +79,11 @@ export class MapCentralesComponent implements OnInit {
         const target = event.target as SVGElement;
         if (target.tagName === 'circle') {
           const info = target.getAttribute('data-info');
+          const potencia_activa = target.getAttribute('potencia_activa');
+          const potencia_reactiva = target.getAttribute('potencia_reactiva');
+          const capacidad_instalada = target.getAttribute('capacidad_instalada');
           if (info) {
-            tooltip.innerHTML = `<b>${info.split(' - ')[0]}</b><br>${info.split(' - ')[1]} <br> ${info.split(' - ')[2]} <br> ${info.split(' - ')[3] || ''}`;
+            tooltip.innerHTML = `<b>${info}</b><br><b>Potencia activa: </b>${potencia_activa} <br><b>Potencia reactiva: </b> ${potencia_reactiva} <br> <b>Capacidad instalada: </b>${capacidad_instalada || ''}`;
             tooltip.style.opacity = '1';
           }
         }
